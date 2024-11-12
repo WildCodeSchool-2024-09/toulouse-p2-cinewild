4dca6f99fc1b790132215fb01b1f43a06f875d0f
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-11-12 11:59:25 +0100

    fix: add p

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
           <p className="block-primary-sub">Votre framework JavaScript</p>
         </hgroup>
         <p>Vous avez lu le README du projet ?!!!</p>
-        <p>Ouais c'est Radwan</p>
+        <p>Ouais c'est Radwan Osman</p>
       </main>
 
       <footer>
