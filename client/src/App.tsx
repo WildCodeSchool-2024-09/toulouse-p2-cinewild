a77d9cc4b15b356dd8a1764eec4fda597e4ffe56
TSMB54 <tom.schricke@gmail.com>
2024-11-12 10:46:34 +0100

    fix: fix title acording to requested changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">OUAIS CEST MICHEL</h1>
+        <h1 className="logo">Ouais C'est Michel</h1>
       </header>
 
       <nav className="navbar">
