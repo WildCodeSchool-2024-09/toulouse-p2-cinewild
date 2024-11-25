7729872727cea2ccda41b98611fb659cf4e0cedd
CharlotteAlbouy <albouy.charlotte@gmail.com>
2024-11-25 10:20:13 +0100

    feat: correcting requested changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,11 @@ import { FavoritesProvider } from "./components/FavoritesContext";
 import HomeScreen from "./components/HomeScreen";
 
 const App: React.FC = () => {
-  return <FavoritesProvider>{<HomeScreen />}</FavoritesProvider>;
+  return (
+    <FavoritesProvider>
+      <HomeScreen />
+    </FavoritesProvider>
+  );
 };
 
 export default App;
